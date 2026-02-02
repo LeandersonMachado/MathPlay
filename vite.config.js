d4d26cf7a1670a037c2e585f9773f5fd7f875dba
LeandersonMachado <leandersonmachado.dev@gmail.com>
2026-02-02 14:31:23 -0300

    fix: Resolve o erro 404 ao atualizar pagina no Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
